19797cb03d20f2e41e335a02c87baa4b46e74755
Rohit Gujar <techguy441@gmail.com>
2023-01-25 13:41:34 +0530

    First Commit by Rohit

---
+++ emp11.sql
@@ -0,0 +1,7 @@
+desc emp1;
+insert into emp1 (empid , name , dept , age ) values ('01' ,'rohit' ,'cs' , '26');
+insert into emp1 (empid ,name , dept , age) values ('02' ,'omkar' ,'bsc' ,'23' );
+insert into emp1 (empid , name , dept , age) values ('03' , 'rahul' ,'cs' , '24');
+insert into emp1(empid, name , dept , age) values ('04' , 'vaibhav' , 'civil' ,'28') ;
+insert into emp1 (empid, name , dept , age) values ('05' , 'sanket' ,'cs' , '26');
+insert into emp1 (empid , name , dept , age) values ('06','saurabh', 'bsc' ,'23');
